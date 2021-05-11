4583331c6147ff596a315ee86a4a69836b87451c
Isaiah Jenkins <icjenkins@dons.usfca.edu>
2021-05-10 18:03:30 -0700

    Updated register

--- server.js
+++ server.js
@@ -2,70 +2,64 @@ const express = require('express');
 const app = express();
 const bcrypt = require('bcryptjs');
 const cors = require('cors');
+const knex = require('knex');
+
+const db = knex({
+	client: 'pg',
+	connection: {
+		host: '127.0.0.1',
+		user: 'isaiahjenkins',
+		password: '',
+		database: 'smart-brain'
+	}
+});
 
 app.use(express.json());
 app.use(cors());
 
-const database = {
-	users: [
-		{
-			id: '123',
-			name: 'John',
-			email: 'john@gmail.com',
-			password: 'cookies',
-			entries: 0,
-			joined: new Date()
-		},
-		{
-			id: '124',
-			name: 'Sally',
-			email: 'sally@gmail.com',
-			password: 'pizza',
-			entries: 0,
-			joined: new Date()
-		}
-	]
-};
 app.get('/', (req, res) => {
-	res.send(database.users);
+	//res.send(database.users);
 });
 
 app.post('/signin', (req, res) => {
-	if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
-		res.json({
-			id: database.users[0].id,
-			name: database.users[0].name,
-			email: database.users[0].email,
-			entries: database.users[0].entries,
-			joined: database.users[0].joined
-		});
-	} else {
-		res.status(400).json('error loggin in');
-	}
+	// if (req.body.email === database.users[0].email && req.body.password === database.users[0].password) {
+	// 	res.json({
+	// 		id: database.users[0].id,
+	// 		name: database.users[0].name,
+	// 		email: database.users[0].email,
+	// 		entries: database.users[0].entries,
+	// 		joined: database.users[0].joined
+	// 	});
+	// } else {
+	// 	res.status(400).json('error loggin in');
+	// }
 });
 
 app.post('/register', (req, res) => {
 	const { email, name, password } = req.body;
 	// bcrypt.hash(password, 8, function (err, hash) {});
-	database.users.push({
-		id: '123',
-		name: name,
-		email: email,
-		entries: 0,
-		joined: new Date()
-	});
-	res.json(database.users[database.users.length - 1]);
+	db('users')
+		.returning('*')
+		.insert({
+			name: name,
+			email: email,
+			joined: new Date()
+		})
+		.then((user) => {
+			res.json(user[0]);
+		})
+		.catch((err) => res.status(400).json('unable to register'));
 });
 
 app.get('/profile/:id', (req, res) => {
 	const { id } = req.params;
 	let found = false;
-	database.users.forEach((user) => {
-		if (user.id === id) {
-			found = true;
-			return res.json(user);
-		}
-	});
+	// database.users.forEach((user) => {
+	// 	if (user.id === id) {
+	// 		found = true;
+	// 		return res.json(user);
+	// 	}
+	// });
 	if (!found) {
 		res.status(404).json('no such user');
 	}
@@ -74,13 +68,13 @@ app.get('/profile/:id', (req, res) => {
 app.put('/image', (req, res) => {
 	const { id } = req.body;
 	let found = false;
-	database.users.forEach((user) => {
-		if (user.id === id) {
-			found = true;
-			user.entries++;
-			return res.json(user.entries);
-		}
-	});
+	// database.users.forEach((user) => {
+	// 	if (user.id === id) {
+	// 		found = true;
+	// 		user.entries++;
+	// 		return res.json(user.entries);
+	// 	}
+	// });
 	if (!found) {
 		res.status(404).json('no such user');
 	}
